3e792dd07fb9efdb29dab45a1c5866aca157cbd4
Lvroosa <luke.roosa1@outlook.com>
2025-05-01 20:40:07 -0500

    Create home.py

---
+++ Main/home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+
+st.set_page_config(page_title="Tulane Risk Dashboard")
+
+
+
+st.title("Welcome to the Tulane Risk Dashboard")
+
+st.write("Use the sidebar to navigate to different sentiment tools:")
+
+st.markdown("- Tweet Analysis\n- News Sentiment")
