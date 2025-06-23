8f0c556cd68cafac5135599ad5b7dd7eea050982
ERSRisk <njenkins4@tulane.edu>
2025-06-23 10:23:02 -0500

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-from selenium.wbdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 import pickle
 import trafilatura
 import os
