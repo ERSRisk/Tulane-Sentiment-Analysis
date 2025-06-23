856a14d94f321f73d0ece1fd93ae1c175cf0c8fe
ERSRisk <njenkins4@tulane.edu>
2025-06-23 15:08:47 -0500

    Update test.py

--- test.py
+++ test.py
@@ -251,7 +251,7 @@ def get_cookies():
     driver.get("https://login.nola.com/u/login?state=hKFo2SB4czdxOUVyNmdrVkY5Q0htcnl5Sk9pU1dEQkFWczlScqFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIHBzOUdTQnBidGg0U3MtM25NdFotZDh6djZLS2tJVHZvo2NpZNkgTmFINm9Od0tubng2eWdIMjdMVFBZMVBBcGtSZE5USlU")
     time.sleep(10)
     driver.save_screenshot('screenshot.png')
-    WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.ID, "username"))).send_keys("Njenkins4@tulane.edu")
+    WebDriverWait(driver, 510).until(EC.presence_of_element_located((By.ID, "username"))).send_keys("Njenkins4@tulane.edu")
     driver.find_element(By.ID, "password").send_keys("ERSDepartment2023")
     driver.find_element(By.CSS_SELECTOR, 'button[type = "submit"]').click()
     time.sleep(60)
