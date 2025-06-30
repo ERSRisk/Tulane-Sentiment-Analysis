63a3454302ec41c38ed03a17252227f5a937957d
ERSRisk <njenkins4@tulane.edu>
2025-06-30 14:02:56 -0500

    Update BERT_update.py

--- Model_training/BERT_update.py
+++ Model_training/BERT_update.py
@@ -60,7 +60,7 @@ else:
         words = topic_model.get_topic(topic)
         docs = topic_model.get_representative_docs()[topic]
         random.shuffle(docs)
-        docs = docs[:15]
+        docs = docs[:10]
         keywords = ', '.join([word for word, _ in words])
         blocks = f"Topic {topic}: Keywords: {keywords}. Representative Documents: {docs[0]} | {docs[1]}"
         topic_blocks.append((topic, blocks))
