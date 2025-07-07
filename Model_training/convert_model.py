b0a5cf6436e2578d14824a3f512caa68521824f6
ERSRisk <njenkins4@tulane.edu>
2025-07-07 15:47:10 -0500

    Update convert_model.py

--- Model_training/convert_model.py
+++ Model_training/convert_model.py
@@ -1,5 +1,5 @@
 from bertopic import BERTopic
 
-model = BERTopic.load('Model_training/BERTopic_model.pkl')
+model = BERTopic.load('Model_training/BERTopic_model', local = True)
 model.save("Model_training/BERTopic_model_persisted", serialization = 'persistence')
 print("Model re-saved")
