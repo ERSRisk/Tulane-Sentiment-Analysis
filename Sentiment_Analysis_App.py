8a16277ec42d1bfc10b145758f1faab0d6be7112
Heber Samuel Centeno Martinez <hcentenomartinez@tulane.edu>
2025-07-25 09:37:56 -0500

    Update Sentiment_Analysis_App.py

--- Sentiment_Analysis_App.py
+++ Sentiment_Analysis_App.py
@@ -860,7 +860,7 @@ if selection == "Article Risk Review":
         st.sidebar.error("Start date must be before end date.")
     # Load articles and risks
     
-    articles = pd.read_csv('BERTopic_results_scored_full.csv')
+    articles = pd.read_csv('BERTopic_results_test.csv')
     #articles = articles[articles['Published']> start_date.strftime('%Y-%m-%d')]
     #articles = articles[articles['Published']< end_date.strftime('%Y-%m-%d')]
     articles = articles[articles['University Label'] == 1]
