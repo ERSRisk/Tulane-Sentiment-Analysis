69a3095e36014284cbef509b53e6f3ecb9560198
ERSRisk <njenkins4@tulane.edu>
2025-08-12 11:59:33 -0500

    Update Sentiment_Analysis_App.py

--- Sentiment_Analysis_App.py
+++ Sentiment_Analysis_App.py
@@ -856,8 +856,8 @@ if selection == "Article Risk Review":
 
     required_keys = {'Title', 'Content'}
     if 'articles' not in st.session_state:
-        if os.path.exists('Model_training/risk_predictions.csv'):
-            results_df = pd.read_csv('Model_training/risk_predictions.csv')
+        if os.path.exists('Model_training/BERTopic_results1.csv'):
+            results_df = pd.read_csv('Model_training/BERTopic_results.csv')
 
             use_changes = Path('Model_training/BERTopic_changes.csv').is_file() and Path('Model_training/BERTopic_changes.csv').stat().st_size > 0
             changes_df = None
