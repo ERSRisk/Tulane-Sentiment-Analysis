b30741f7d4d060b26f9457cc27c7fe96dc67ece4
ERSRisk <njenkins4@tulane.edu>
2025-08-12 11:22:26 -0500

    Update Sentiment_Analysis_App.py

--- Sentiment_Analysis_App.py
+++ Sentiment_Analysis_App.py
@@ -853,7 +853,7 @@ if selection == "Article Risk Review":
     import os
     from pathlib import Path
     import ast
-
+    st.dataframe('Model_training/risk_predictions.csv')
     if 'articles' not in st.session_state:
         if os.path.exists('Model_training/risk_predictions.csv'):
             results_df = pd.read_csv('Model_training/risk_predictions.csv')
