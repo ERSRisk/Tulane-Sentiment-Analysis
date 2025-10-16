4a20562ba202b2dfebe4ec8e5156849c8abc82a1
ERSRisk <njenkins4@tulane.edu>
2025-10-15 20:54:27 -0500

    Update BERT_update.py

--- Model_training/BERT_update.py
+++ Model_training/BERT_update.py
@@ -1915,7 +1915,7 @@ def load_midstep_from_release(local_cache_path = 'Model_training/Step1.csv.gz'):
 #results_df = load_university_label(df)
 #results_df = results_df.drop(columns = ['Acceleration_value_x', 'Acceleration_value_y'], errors = 'ignore')
 #atomic_write_csv('Model_training/initial_label.csv.gz', results_df, compress = True)
-df = pd.read_csv('Model_training/label_')
+df = pd.read_csv('Model_training/label_initial.csv.gz', compression = 'gzip')
 df = risk_weights(results_df)
 df = df.drop(columns = ['University Label_x', 'University Label_y'], errors = 'ignore')
 #atomic_write_csv("Model_training/BERTopic_results2.csv.gz", df, compress=True)
