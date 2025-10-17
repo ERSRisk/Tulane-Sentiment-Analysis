9bfa201dd0deafb2b82939b27d5a2dd62a4f7cf6
ERSRisk <njenkins4@tulane.edu>
2025-10-16 19:53:41 -0500

    Update RSS_extraction.py

--- Online_Extraction/RSS_extraction.py
+++ Online_Extraction/RSS_extraction.py
@@ -584,7 +584,8 @@ def homeland_sec():
   nola_rss = []
   for link in links:
       link = requote_uri(link)
-      print(link, flush = True)
+      if not link or "nola.gov" not in link.lower():
+        continue
       downloaded = trafilatura.fetch_url(link)
       soup = BeautifulSoup(downloaded, 'html.parser')
       title = soup.find('h2', class_='mt-0').get_text(strip=True) if soup.find('h2', class_='mt-0') else 'No Title Found'
